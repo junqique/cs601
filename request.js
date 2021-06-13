431da731a3f19ab4f69244d11265e4a969352025
junqique <35582082+junqique@users.noreply.github.com>
2021-06-12 23:38:14 -0500

    Add files via upload

---
+++ request.js
@@ -0,0 +1,42 @@
+// const oldButton = document.getElementById('oldButton');
+// const fetchButton = document.getElementById('fetchButton');
+
+
+// const getDegreesOld = () =>{
+//     // const xhr = new XMLHttpRequest();
+//     // xhr.open('get','degrees.json',true);
+//     // xhr.onreadystatechange = () =>{
+//     //     if((xhr.readyState === XMLHttpRequest.DONE) && (xhr.status === 200)){
+//     //         const response = JSON.parse(xhr.responseText);
+//     //         const toInsert = buildDegrees(response);
+//     //     }
+
+//     //     oldButton.insertAdjacentHTML('beforebegin',toInsert);
+//     //     oldButton.style.display = 'none';
+//     // }
+//     // xhr.send();
+//     console.log("hi");
+// }
+
+// const getDegreeNew = () =>{
+//     // fetch('../degrees.json')
+//     //     .then(response => response.json())
+//     //     .then(data => {
+//     //         const toInsert = buildDegrees(data);
+
+//     //         fetchButton.insertAdjacentHTML('beforebegin',toInsert);
+//     //         fetchButton.style.display = 'none';
+//     //     })
+//     console.log("hi");
+// }
+
+// oldButton.addEventListener('click',getDegreesOld());
+// fetchButton.addEventListener('click',getDegreeNew());
+
+// // const buildDegrees = ({degrees}) =>{
+// //     let degreesOutput = '';
+// //     degreesOutput += `<p>${degrees[1].degree.Program}</p>`;
+
+// //     return degreesOutput;
+// // }
+
